95164b0a44f8944ea438ddee43a3a55a07ee29b0
Nazmuz-Saif <nazmuzsaif4080@gmail.com>
2024-09-14 00:41:05 +0600

    write1

--- Reverse_array.c
+++ Reverse_array.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    int arr[]={4,5,1,2};
-    
+    int i,arr[]={4,5,1,2};
+
+    printf("Reverse array: ");
+
+    for(i=3;i>=0;i--){
+        printf("%d\t", arr[i]);
+    }
+    return 0;
 }
