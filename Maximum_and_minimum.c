1cd0e02683415ab6326e8627d5427409a7499c5f
Nazmuz-Saif <nazmuzsaif4080@gmail.com>
2024-09-14 01:05:49 +0600

    first commit

--- Maximum_and_minimum.c
+++ Maximum_and_minimum.c
@@ -1,9 +1,22 @@
 #include <stdio.h>
 int main()
 {
-    int a;
+    int a[5]={40,70,40,50,60};
+    int max,min,i;
+    max=a[0];
+    min=a[0];
 
-
+    for(i=0;i<=4;i++){
+        if(a[i]>max){
+                max=a[i];
+            }
+    }
+    for(i=0;i<=4;i++){
+        if(a[i]<min){
+                min=a[i];
+            }
+    }
+    printf("maximum is %d\tminimum is %d",max,min);
 
     return 0;
 }
